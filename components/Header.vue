c076b6f372e07e97965ad7839d287bec9215f336
Melanisiti <144877273+Melanisiti@users.noreply.github.com>
2024-02-28 15:20:06 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/LOGO.png" alt="logo SMKN 4">
+    </div>
+    <div class="col-lg-11">
+      <h1>Perpus Digital</h1>
+      <h3>SMKN 4 Tasikmalaya</h3>
+      <address>
+        Jl.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="comtainer-fluid">
+    <Header /> <!-- custom component -->
+    <slot /> <!-- page akan mengisi bagian ini -->
+  </div>
+</template>
