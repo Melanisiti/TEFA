f66fb7a8fe0956fcce51c34b616580bc652fe51a
Melanisiti <144877273+Melanisiti@users.noreply.github.com>
2024-03-22 08:59:52 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<!-- <template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/LOGO.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus Digital</h1>
+      <address>
+        SMKN 4 Tasikmalaya<br>Jl.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+    </div>
+</template> -->
+<template>
+<nav class="navbar bg-primary">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+    <img src="~/assets/img/LOGO.png" alt="Bootstrap" width="80">
+  </a>
+  <div class="title me-auto">
+  <h3 class="m-0">Perpus Digital</h3>
+  <address class="m-0">
+  SMKN 4 Tasikmalaya<br>Jl.Depok, Sukamenak, Purbaratu
+  </address>
+  </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+.logo{
+  width: 100px;
+}
+</style> 
