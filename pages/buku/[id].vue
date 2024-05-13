27f91a12d6e7a3b5543f322cda4ac1d46a8f2470
Melanisiti <144877273+Melanisiti@users.noreply.github.com>
2024-05-13 15:31:17 +0700

    Add files via upload

---
+++ pages/buku/[id].vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container-fluid">
+    <div class="row mt-4 d-flex justify-content-evenly">
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <img src="~/assets/img/cover1.jpg" width="220px" height="340px" class="cover" alt="cover1">
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <h5>Penulis: {{ Buku.penulis }}</h5>
+        <h5>Penerbit: {{ Buku.penerbit }}</h5>
+        <h5>Tahun terbit: {{ Buku.tahun_terbit }}</h5>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <h2>Sinopsis</h2>
+      <p class="mt-3"> Pada awalnya Putri yang selalu mengumbar-ngumbar kedekatannya dengan Rio. 
+        Kedekatan itu hanya berlangsung di dunia maya. Dan sangat berbeda di dunia nyata. Mereka berdua jauh dari kata dekat.
+Setelah Putri, Nana juga merasakan hal yang sama. 
+Yang awalnya bahagia bisa dekat dengan Rio. 
+Namun, lagi-lagi kedekatan itu hanya di dunia maya dan bahkan hanya sementara saja. 
+Cerita tersebut sangat menguji pertemanan dua anak manusia yang dimabuk cinta tersebut.</p>
+    </div>
+    <nuxt-link to="/buku">
+      <button type="button" class="btn btn-dark mt-5">Kembali</button>
+    </nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 255px;
+  height: 370px;
+  box-shadow: 1px 1px 10px #424242;
+}
+</style>
+
+<script setup>
+const supabase = useSupabaseClient()
+
+const route = useRoute()
+const buku = ref([])
+
+const getBookById = async () => {
+  const { data, error } = await supabase.from('buku').select(`*, kategori(*)`)
+  .eq('id', route.params.id)
+  if(data) buku.value = data[0]
+}
+
+onMounted(() => {
+  getBookById()
+})
+</script>
